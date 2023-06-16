baae195b73c1a0dc493106a22348c757bcd2e991
damccull <401104+damccull@users.noreply.github.com>
2023-06-16 15:06:46 -0400

    Fix a warning by rearranging some things.

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,6 +1,6 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
+local on_attach = lspconfig.on_attach
+local capabilities = lspconfig.capabilities
 
 local servers = { "html", "cssls", "sqlls" }
 
--- configs/rust-tools.lua
+++ configs/rust-tools.lua
@@ -1,7 +1,6 @@
-local on_attach = require("plugins.configs.lspconfig").on_attach
-local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
-local util = require "lspconfig/util"
+local on_attach = lspconfig.on_attach
+local capabilities = lspconfig.capabilities
 
 local options = {
   server = {
