828948be87b801c553c9c4325cdc4b46e6cfa803
damccull <401104+damccull@users.noreply.github.com>
2023-06-15 23:20:55 -0400

    Try to add formatters

--- plugins.lua
+++ plugins.lua
@@ -1,15 +1,44 @@
 local plugins = {
   {
+    -- Mason plugin downloader
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "html-lsp",
+        "yaml-language-server",
         "rust-analyzer",
-        "rust-lang/rust.vim"
+        "rust-lang/rust.vim",
+        "sqlls",
+        "stylua",
+        "nvim-lua/plenary.nvim"
       },
     },
   },
   {
+    "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "vim",
+                "lua",
+                "rust",
+                "sql",
+                "css",
+                "html",
+                "javascript",
+                "typescript",
+                "json",
+            }
+        }
+    },
+  {
+    -- lsp configurator
     "neovim/nvim-lspconfig",
+    dependencies = {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            require "custom.configs.null-ls"
+        end,
+    },
     config = function()
       require "plugins.configs.lspconfig"
       require "custom.configs.lspconfig"
@@ -23,6 +52,7 @@ local plugins = {
     end,
   },
   {
+    -- RUST TOOLS (includes lsp)
     "simrat39/rust-tools.nvim",
     ft = "rust",
     dependencies = "neovim/nvim-lspconfig",
@@ -33,13 +63,13 @@ local plugins = {
       require('rust-tools').setup(opts)
     end,
   },
-  {
-    "folke/trouble.nvim",
-    cmd = "Trouble",
-    config = function()
-        require("trouble").setup()
-    end,
-  },
+  -- {
+  --   "folke/trouble.nvim",
+  --   cmd = "Trouble",
+  --   config = function()
+  --       require("trouble").setup()
+  --   end,
+  -- },
   {
     "saecki/crates.nvim",
     ft = {"toml"},
@@ -57,33 +87,33 @@ local plugins = {
       return M
     end,
   },
-  {
-    "esensar/nvim-dev-container",
-    opts = {
-      ensure_installed = {
-        "nvim-treesitter/nvim-treesitter"
-      },
-    },
-    lazy = false,
-    config = function()
-      require("devcontainer").setup{
-          attach_mounts = {
-            neovim_config = {
-              enabled = true,
-              options = { "readonly" }
-            },
-            neovim_data = {
-              enabled = false,
-              options = {}
-            },
-            neovim_state = {
-              enabled = false,
-              options = {}
-            },
-          },
-        }
-    end
-  },
+  -- {
+  --   "esensar/nvim-dev-container",
+  --   opts = {
+  --     ensure_installed = {
+  --       "nvim-treesitter/nvim-treesitter"
+  --     },
+  --   },
+  --   lazy = false,
+  --   config = function()
+  --     require("devcontainer").setup{
+  --         attach_mounts = {
+  --           neovim_config = {
+  --             enabled = true,
+  --             options = { "readonly" }
+  --           },
+  --           neovim_data = {
+  --             enabled = false,
+  --             options = {}
+  --           },
+  --           neovim_state = {
+  --             enabled = false,
+  --             options = {}
+  --           },
+  --         },
+  --       }
+  --   end
+  -- },
     {
       "mbbill/undotree",
       lazy = false,
